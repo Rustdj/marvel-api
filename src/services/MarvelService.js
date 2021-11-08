007cc197a18ff344b01d31e2a8f401a7e8a1e718
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-11-08 23:05:50 +0300

    create new project

---
+++ src/services/MarvelService.js
@@ -0,0 +1,13 @@
+
+
+class MarvelServise {
+    getResource(url) {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+}
