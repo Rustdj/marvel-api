67b52f63db21fd94a98daa84b8a2a0cee0b79db5
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-11-09 23:14:41 +0300

    create spinner

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,8 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=66de0ee82b3a175e66c2c35b03d1e2d1';
 
-
-class MarvelServise {
-    getResource(url) {
+    getResource = async (url) => {
         let res = await fetch(url);
 
         if (!res.ok) {
@@ -10,4 +11,26 @@ class MarvelServise {
 
         return await res.json();
     }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacters = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 }
+
+export default MarvelService;
